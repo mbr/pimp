ad6d4df2c458b11d09810fd576ced7950ba582dc
Marc Brinkmann <git@marcbrinkmann.de>
2013-03-02 17:45:02 +0100

    Add packaging.

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='pimp',
+    version='0.1dev',
+    description='pimp installs magic packages. Install rpm packages from PyPi. No refunds.',
+    long_description=read('README.rst'),
+    author='Marc Brinkmann',
+    author_email='git@marcbrinkmann.de',
+    url='http://github.com/mbr/pimp',
+    license='MIT',
+    packages=find_packages(exclude=['test']),
+    entry_points={
+        'console_scripts': [
+            'pimp = pimp:main',
+        ],
+    }
+)
